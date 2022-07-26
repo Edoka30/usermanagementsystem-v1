3307e13b4beb974902f7248e2e985ee122b81a0f
Thomas <thomasedoka30@gmail.com>
2022-07-26 19:10:40 +0100

    added component scan

--- src/main/java/com/UserMgtSystem/UserMgtSystem/UserMgtSystemApplication.java
+++ src/main/java/com/UserMgtSystem/UserMgtSystem/UserMgtSystemApplication.java
@@ -2,9 +2,11 @@ package com.UserMgtSystem.UserMgtSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication 
+@ComponentScan (basePackages = "com.UserMgtSystem.UserMgtSystem.ServiceImpl"+"com.UserMgtSystem.UserMgtSystem.Controller")
 public class UserMgtSystemApplication {
 
 	public static void main(String[] args) {
